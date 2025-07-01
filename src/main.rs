e8c29feb20fe134103d1472b1eedaf89e5c7cca8
thijs <thijs@licht51.de>
2025-07-01 20:22:30 +0200

    bubble sort for slice

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+
+const LIST_LEN: usize = 100000;
+fn main() {
+    let mut rand_array: [u64; LIST_LEN] = rand::random();
+
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    bubble_sort(&mut rand_array);
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+
+    let duration = end - start;
+
+    println!("Took {}ms for {} elements.", duration.as_millis(), LIST_LEN)
+
+}
+
+fn bubble_sort(arr: &mut [u64]) {
+    let last_check = arr.len();
+
+    for end_index in (1..last_check).rev() {
+        for i in 0..end_index {
+            if arr[i] > arr[i+1] {
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+    
+}
